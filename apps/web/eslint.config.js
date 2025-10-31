6b2e929410b308560e5707a19cb5ab39210efcf1
AleksVale <alexalexx3@gmail.com>
2025-10-31 14:48:39 -0300

    Refactor code structure for improved readability and maintainability
    Signed-off-by: AleksVale <alexalexx3@gmail.com>

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,4 +1,20 @@
-import { nextJsConfig } from "@repo/eslint-config/next-js";
-
-/** @type {import("eslint").Linter.Config[]} */
-export default nextJsConfig;
+const base = require('../../packages/eslint-config/base.js');
+module.exports = {
+	...base,
+	extends: [
+		...(base.extends || []),
+		'plugin:react/recommended',
+		'plugin:react-hooks/recommended',
+	],
+	plugins: [
+		...(base.plugins || []),
+		'react',
+		'react-hooks',
+	],
+	settings: {
+		...(base.settings || {}),
+		react: {
+			version: 'detect',
+		},
+	},
+};
